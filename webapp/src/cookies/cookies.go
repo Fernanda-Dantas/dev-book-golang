20e6ae820852dddc7abaa3b4836e6ff52b112122
fernanda-dantas-indico <fernanda.dantas@indico.net.br>
2025-10-28 17:31:39 -0300

    middlewares e requisicao de cookies

--- webapp/src/cookies/cookies.go
+++ webapp/src/cookies/cookies.go
@@ -17,7 +17,7 @@ func Configurar() {
 // Registra as informações de autenticação
 func Salvar(w http.ResponseWriter, ID, token string) error {
 	dados := map[string]string{
-		"id": ID,
+		"id":    ID,
 		"token": token,
 	}
 
@@ -27,12 +27,27 @@ func Salvar(w http.ResponseWriter, ID, token string) error {
 	}
 
 	http.SetCookie(w, &http.Cookie{
-		Name: "dados",
-		Value: dadosCodificados,
-		Path: "/",
+		Name:     "dados",
+		Value:    dadosCodificados,
+		Path:     "/",
 		HttpOnly: true,
 	})
 
 	return nil
+
+}
+
+func Ler(r *http.Request) (map[string]string, error) {
+	cookie, erro := r.Cookie("dados")
+	if erro != nil {
+		return nil, erro
+	}
+
+	valores := make(map[string]string)
+	if erro = s.Decode("dados", cookie.Value, &valores); erro != nil {
+		return nil, erro
+	}
+
+	return valores, nil
 
 }
