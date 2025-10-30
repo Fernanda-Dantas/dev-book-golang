43c7f4f37670a33fcb975b78d20df89dbaed5eb0
fernanda-dantas-indico <fernanda.dantas@indico.net.br>
2025-10-30 15:15:36 -0300

    add criar publicacoes logica no front

--- webapp/src/utils/templates.go
+++ webapp/src/utils/templates.go
@@ -10,6 +10,7 @@ var templates *template.Template
 // Insere os templates html na variável templates
 func CarregarTemplates() {
 	templates = template.Must(template.ParseGlob("views/*.html"))
+	templates = template.Must(templates.ParseGlob("views/templates/*.html"))
 }
 
 // Renderiza uma página html na tela
