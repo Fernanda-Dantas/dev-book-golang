35757cbb074d8b6dd291d52b931771b05a4f9394
fernanda-dantas-indico <fernanda.dantas@indico.net.br>
2025-10-31 14:52:50 -0300

    add pagina de atualizar publicacao

--- webapp/assets/js/publicacoes.js
+++ webapp/assets/js/publicacoes.js
@@ -1,4 +1,8 @@
 $('#nova-publicacao').on('submit', criarPublicacao);
+$('.curtir-publicacao').on('click', curtirPublicacao);
+
+$(document).on('click', '.curtir-publicacao', curtirPublicacao);
+$(document).on('click', '.descurtir-publicacao', descurtirPublicacao);
 
 function criarPublicacao(evento) {
   evento.preventDefault();
@@ -15,4 +19,59 @@ function criarPublicacao(evento) {
   }).fail(function() {
     alert("Erro ao criar publicação!");
   })
+}
+
+function curtirPublicacao(event) {
+  event.preventDefault();
+
+  const click = $(event.target);
+  const publicacaoId = click.closest('div').data('publicacao-id');
+
+  click.prop('disabled', true);
+  $.ajax({
+    url: `/publicacoes/${publicacaoId}/curtir`,
+    method: "POST"
+  }).done(function() {
+    const contadorDeCurtidas = click.next('span');
+    const quantidadeDeCurtidas = parseInt(contadorDeCurtidas.text());
+
+    contadorDeCurtidas.text(quantidadeDeCurtidas + 1);
+
+    click.addClass('descurtir-publicacao');
+    click.addClass('text-danger');
+    click.removeClass('curtir-publicacao');
+
+  }).fail(function() {
+    alert('Erro ao curtir publicação');
+  }).always(function() {
+    click.prop('disabled', false);
+  });
+
+}
+
+function descurtirPublicacao(event) {
+    event.preventDefault();
+
+  const click = $(event.target);
+  const publicacaoId = click.closest('div').data('publicacao-id');
+
+  click.prop('disabled', true);
+  $.ajax({
+    url: `/publicacoes/${publicacaoId}/descurtir`,
+    method: "POST"
+  }).done(function() {
+    const contadorDeCurtidas = click.next('span');
+    const quantidadeDeCurtidas = parseInt(contadorDeCurtidas.text());
+
+    contadorDeCurtidas.text(quantidadeDeCurtidas - 1);
+
+    click.removeClass('descurtir-publicacao');
+    click.removeClass('text-danger');
+    click.addClass('curtir-publicacao');
+
+  }).fail(function() {
+    alert('Erro ao curtir publicação');
+  }).always(function() {
+    click.prop('disabled', false);
+  });
 }
